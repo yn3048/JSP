1b51999f58df747d133e1be1547ea183b5ec995c
dpsk08270 <dpsk08270@gmail.com>
2024-01-30 18:44:04 +0900

    교재예제

---
+++ Textbook/src/main/java/com/ssamz/web/user/LoginServlet.java
@@ -0,0 +1,27 @@
+package com.ssamz.web.user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public LoginServlet() {
+    	System.out.println("==> LoginServlet 생성");
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
+		System.out.println("--->GET 방식의 요청 처리");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
+		System.out.println("---> POST 방식의 요청 처리");
+	}
+
+}
